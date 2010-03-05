080ca5adec00c87c37b9000520205f8e52128834
Eito Katagiri <eitoball@gmail.com>
2010-03-05 14:46:39 +0900

    Fixed extconf.rb to be able to search include and lib path dynamically

--- extconf.rb
+++ extconf.rb
@@ -1,5 +1,5 @@
 require "mkmf"
 
-$CFLAGS += " -I /usr/local/include "
-$LOCAL_LIBS += " -L /usr/local/lib -lkakasi "
-create_makefile("kakasi")
+if have_header("libkakasi.h") && have_library("kakasi", "kakasi_do")
+  create_makefile("kakasi")
+end
